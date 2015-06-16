c69c5bf4065d92aa6ea643909ae98bc0e3c5e7f0
ashes999 <ashes999@yahoo.com>
2015-06-16 13:05:13 -0400

    First cut generating templates and building code

---
+++ proof-of-concept/build.rb
@@ -0,0 +1,87 @@
+class Builder
+  require 'fileutils'
+  
+  # The code 
+  ENTRY_POINT = 'main.rb'
+  HTML_TEMPLATE = 'template.html'
+  CODE_EXCLUSIONS = ['build.rb']
+  OUTPUT_DIR = 'bin'
+  OUTPUT_FILE = 'index.html'
+  DIRECTORY_EXCLUSIONS = [OUTPUT_DIR]
+  SOURCE_PLACEHOLDER = 'puts \'Put your code in .rb files, not here\''  
+  
+  # Builds and combines all ruby files; generates final output HTML/project
+	def build
+		ensure_build_files_exist
+    ensure_source_placeholder_exists
+    code = amalgamate_code_files
+    build_result(code)
+	end
+  
+  private
+  
+  # Make sure our build files exist on disk
+  def ensure_build_files_exist
+    ensure_file_exists(ENTRY_POINT)
+    ensure_file_exists(HTML_TEMPLATE)
+  end
+  
+  # Check if a file exists and raise if it doesn't.
+  def ensure_file_exists(filename)
+    raise "#{filename} not found" unless File.exist?(filename)
+  end
+  
+  def ensure_source_placeholder_exists
+    content = File.read(HTML_TEMPLATE)
+    raise "Template #{HTML_TEMPLATE} doesn't include placeholder #{SOURCE_PLACEHOLDER}" unless content.include?(SOURCE_PLACEHOLDER)
+  end
+  
+  # Combine all Ruby files together; append the ENTRY_POINT last.
+  def amalgamate_code_files
+    final_code = ''
+    files = Dir.glob('**/*.rb')
+    
+    # TODO: what order do we traverse? These are alphabetical, not even listed
+    # by directory/subdirectory first. Should we build a graph of dependencies?
+    # TODO: do we need to append the entry point last? Why not just leave it up to the user?
+    files.each do |f|
+      next if CODE_EXCLUSIONS.include?(f)
+      file_code = File.read(f)
+      final_code = "#{final_code}#{file_code}"
+      puts "Processed #{f}"
+    end
+    
+    return final_code
+  end
+  
+  # Builds the final project output, using the template and amalgamated code.
+  # Also copies relevant, dependency directories.
+  def build_result(code)
+    # TODO: incremental build?
+    
+    puts "Building to #{OUTPUT_DIR} ..."    
+    FileUtils.rm_rf(OUTPUT_DIR) if Dir.exist?(OUTPUT_DIR)
+    FileUtils.mkdir_p(OUTPUT_DIR)
+    
+    template_with_code = File.read(HTML_TEMPLATE).sub(SOURCE_PLACEHOLDER, code)
+    File.open("#{OUTPUT_DIR}/#{OUTPUT_FILE}", 'w') { |f|
+      f.write(template_with_code)
+    }
+    puts "Wrote HTML and code to #{OUTPUT_DIR}/#{HTML_TEMPLATE}"
+    
+    directories = Dir.glob('**/*/')    
+    directories.each do |d|
+      d = d[0, d.rindex('/')] # remove trailing slash      
+      if DIRECTORY_EXCLUSIONS.include?(d)
+        puts "Skipping #{d}"
+        next
+      end      
+      FileUtils.cp_r(d, "#{OUTPUT_DIR}/#{d}")
+      puts "Copied #{d} directory"
+    end
+    
+    puts "Done!"
+  end
+end
+
+Builder.new.build
--- proof-of-concept/main.rb
+++ proof-of-concept/main.rb
@@ -4,7 +4,21 @@
 class Main
   def run
     $window = MrubyJs::get_root_object
-    puts "Window is $window"
-    puts "The time is now #{Time.new}"
+    $crafty = $window.Crafty
+    Game.new
+    puts "Done at #{Time.new}"
   end
 end
+
+class Game
+  def initialize
+    $crafty.init(800, 600)
+    $crafty.background('black')
+    e = $crafty.e('2D, Canvas, Color, Alpha, Fourway')
+    e.attr({ :w => 32, :h => 32 })
+    e.color('red')
+    e.fourway(8)
+  end
+end
+
+Main.new.run
