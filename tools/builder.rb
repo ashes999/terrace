57832d0544a2cc8448f7aab531c62f84dbddee47
ashes999 <ashes999@yahoo.com>
2015-06-22 18:16:12 -0400

    Copy over CONTENT_FOLDER during build

--- tools/builder.rb
+++ tools/builder.rb
@@ -1,13 +1,25 @@
 class Builder
   require 'fileutils'
   require './tools/web_craftyjs_builder'
-  
-  ENTRY_POINT = 'main.rb'    
+
+  # Folder to copy. Contains all the content (images, audio, etc.)
+  CONTENT_FOLDER = 'content'
+
+  # File that contains our main / entry-point, to start our game
+  ENTRY_POINT = 'main.rb'
+
+  # When copying code, exclude these files.
   CODE_EXCLUSIONS = ['build.rb', ENTRY_POINT ] # ENTRY_POINT is added last
+
+  # Location of built/generated output/binaries
   OUTPUT_DIR = 'bin'
+
+  # Supported targets
   TARGETS = { 'web-craftyjs' => 'WebCraftyJsBuilder' }
+
+  # Location of target-specific source
   TARGETS_FOLDER = 'lib'
-  
+
   # Builds and combines all ruby files; generates final output project
 	def build
     if ARGV[0].nil?
@@ -16,37 +28,38 @@ class Builder
       @target = ARGV[0]
       raise "#{ARGV[0]} target is not supported" if @target.nil?
     end
-    
-    ensure_file_exists(ENTRY_POINT)    
+
+    ensure_file_exists(ENTRY_POINT)
     puts "Buidling #{@target} target ..."
-    
+
     builder_class = Object.const_get(TARGETS[@target])
     @builder = builder_class.new({
       :source_folder => "#{TARGETS_FOLDER}/#{@target}",
-      :output_folder => "#{OUTPUT_DIR}/#{@target}"
+      :output_folder => "#{OUTPUT_DIR}/#{@target}",
+      :content_folder => CONTENT_FOLDER
     })
     @code = amalgamate_code_files
     build_result
 	end
-  
+
   protected
-  
+
   # Check if a file exists and raise if it doesn't.
   def ensure_file_exists(filename)
     raise "#{filename} not found" unless File.exist?(filename)
   end
-  
+
   private
-  
+
   # Combine all Ruby files together.
   def amalgamate_code_files
     print 'Concatenating ruby files '
     final_code = ''
-            
-    files = Dir.glob('lib/common/**/*.rb') + # generic code  
+
+    files = Dir.glob('lib/common/**/*.rb') + # generic code
       Dir.glob("lib/#{@target}/**/*.rb") + # target-specific code
-      Dir.glob('src/**/*.rb') 
-    
+      Dir.glob('src/**/*.rb')
+
     # TODO: what order do we traverse? These are alphabetical, not even listed
     # by directory/subdirectory first. Should we build a graph of dependencies?
     # TODO: do we need to append the entry point last? Why not just leave it up to the user?
@@ -55,24 +68,24 @@ class Builder
       final_code = "#{final_code}\n#{file_code}"
       print '.'
     end
-    
+
     # Add entry-point code lasts, since it depends on everything else
     entry_point = File.read(ENTRY_POINT)
     final_code = "#{final_code}\n#{entry_point}"
-    
+
     FileUtils.rm_f "#{OUTPUT_DIR}/#{ENTRY_POINT}"
     File.open("#{OUTPUT_DIR}/#{ENTRY_POINT}", 'w') { |f|
       f.write(final_code)
     }
-    
+
     puts ' done.'
     return final_code
   end
-  
+
   # Builds the final project output, using the template and amalgamated code.
   # Also copies relevant, dependency directories.
   def build_result
-    puts "Building to #{OUTPUT_DIR} ..."    
+    puts "Building to #{OUTPUT_DIR} ..."
     @builder.build(@code)
     puts "Done!"
   end
--- tools/web_craftyjs_builder.rb
+++ tools/web_craftyjs_builder.rb
@@ -6,11 +6,12 @@ class WebCraftyJsBuilder < Builder
   SOURCE_PLACEHOLDER = 'puts \'Put your code in .rb files, not here\''
   WEBRUBY_PLACEHOLDER = 'src="lib/webruby.js"'
   TARGET = 'web-craftyjs'
-  
+
   def initialize(args)
     @source_folder = args[:source_folder]
     @output_folder = args[:output_folder]
-    
+    @content_folder = args[:content_folder]
+
     mode = ARGV[1] || "debug"
     raise "Can't build in '#{mode}' mode" if WEBRUBY_FILES[mode.to_sym].nil?
     puts "Building in #{mode} mode"
@@ -18,31 +19,35 @@ class WebCraftyJsBuilder < Builder
 		ensure_build_files_exist
     ensure_source_placeholder_exists
   end
-  
+
   def build(code)
   # Substitute code into the template
     template_with_code = File.read("#{@source_folder}/#{HTML_TEMPLATE}").sub(SOURCE_PLACEHOLDER, code)
-    
+
     # Specify debug/release version of webruby
     template_with_code = template_with_code.sub(WEBRUBY_PLACEHOLDER, WEBRUBY_PLACEHOLDER.sub('.js', "-#{@mode}.js"))
-    
+
     # Start writing out files. This is always a "clean" build.
     FileUtils.rm_rf @output_folder
-    FileUtils.mkdir_p @output_folder    
+    FileUtils.mkdir_p @output_folder
+
+    # Copy content
+    FileUtils.cp_r CONTENT_FOLDER, "#{@output_folder}"
+
+    # Copy main code blob
     File.open("#{@output_folder}/#{OUTPUT_FILE}", 'w') { |f|
       f.write(template_with_code)
     }
-    
     FileUtils.cp_r("#{@source_folder}/lib", "#{@output_folder}/lib")
-    
+
     # Keep one of: webruby-debug or webruby-release
     delete = @mode == :debug ? WEBRUBY_FILES[:release] : WEBRUBY_FILES[:debug]
     delete = "#{@output_folder}/#{delete}"
     FileUtils.rm_f delete
   end
-  
+
   private
-  
+
   # Make sure our build files exist on disk
   def ensure_build_files_exist
     ensure_file_exists("#{@source_folder}/#{HTML_TEMPLATE}")
@@ -51,7 +56,7 @@ class WebCraftyJsBuilder < Builder
       ensure_file_exists("#{@source_folder}/#{file}")
     end
   end
-  
+
   def ensure_source_placeholder_exists
     content = File.read("#{@source_folder}/#{HTML_TEMPLATE}")
     raise "Template #{@source_folder}/#{HTML_TEMPLATE} doesn't include placeholder #{SOURCE_PLACEHOLDER}" unless content.include?(SOURCE_PLACEHOLDER)
