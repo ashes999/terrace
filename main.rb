52c20e672b841eea04e0f4f53cda7868d0f515f9
ashes999 <ashes999@yahoo.com>
2015-06-25 15:12:15 -0400

    Desktop: Rename gosu PoC; re-add web main file.

--- main.rb
+++ main.rb
@@ -5,111 +5,25 @@
 # Load the terrace library, and target-specific code. Do not remove these!
 #= require ./lib/common/terrace_common.rb
 #= require ./lib/TARGET/terrace.rb
+g = Game.new(800, 600)
+g.load({
+  :images => ['content/images/fox.png', 'content/images/emblem.png'],
+  :audio => ['content/audio/noise.ogg']
+}, lambda {
 
-require 'rubygems'
-require 'gosu'
-require './lib/common/core/entity'
+  touches = 0
+  t = Entity.new(TextComponent.new)
+  t.text('Touches: 0')
+  t.move(8, 8)
 
-class GameWindow < Gosu::Window
+  e = Entity.new(ImageComponent.new, KeyboardComponent.new, TwoDComponent.new, TouchComponent.new, AudioComponent.new)
+  e.image('content/images/fox.png')
+  e.move_with_keyboard
 
-  def initialize
-    super(800, 600,false)
-    self.caption = "Desktop Target"
-    e = Entity.new(ImageComponent.new(self), KeyboardComponent.new, TwoDComponent.new)
-    e.image('content/images/fox.png')
-    e.move_with_keyboard
-  end
-
-  def update
-    # called every update tick
-  end
-
-  def draw
-    ImageComponent::all.each do |i|
-      i.draw
-    end
-  end
-
-  def button_down(id)
-    KeyboardComponent.all.each do |k|
-      k.on_key(id)
-    end
-
-    case id
-      when Gosu::KbEscape
-        close  # exit on press of escape key
-    end
-  end
-end
-
-class BaseComponent
-  attr_accessor :entity
-end
-
-class TwoDComponent < BaseComponent
-  attr_accessor :x, :y, :z
-
-  def initialize
-    @x = @y = @z = 0
-  end
-
-  # def size(width, height)
-  # def color(color)
-
-  def move(x, y)
-    @x = x
-    @y = y
-  end
-end
-
-class ImageComponent < BaseComponent
-
-  @@all = []
-
-  def self.all
-    return @@all
-  end
-
-  def initialize(window)
-    @window = window
-  end
-
-  def image(string)
-    @image = Gosu::Image.new(@window, string, false)
-    @@all << self
-  end
-
-  def draw
-    @image.draw(@entity.x, @entity.y, @entity.z) # TODO: use Z (currently 0)
-  end
-end
-
-class KeyboardComponent < BaseComponent
-  @@all = []
-
-  def self.all
-    return @@all
-  end
-
-  def move_with_keyboard
-    @@all << self
-  end
-
-  # internal
-
-  def on_key(key)
-    case key
-      when Gosu::KbRight
-        @entity.x += 8
-      when Gosu::KbDown
-        @entity.y += 8
-      when Gosu::KbLeft
-        @entity.x -= 8
-      when Gosu::KbUp
-        @entity.y -= 8
-    end
-  end
-end
-
-window = GameWindow.new
-window.show
+  e.touch(lambda {
+    e.move(e.x + 100, e.y + 100)
+    e.play('content/audio/noise.ogg')
+    touches += 1
+    t.text("Touches: #{touches}")
+  })
+})
