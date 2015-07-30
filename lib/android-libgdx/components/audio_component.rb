d43b806bc2e82893cd83a816334e0de0763f2688
ashes999 <ashes999@yahoo.com>
2015-07-30 09:45:50 -0400

    Fix audio looping for #3

--- lib/android-libgdx/components/audio_component.rb
+++ lib/android-libgdx/components/audio_component.rb
@@ -3,8 +3,14 @@ class AudioComponent < BaseComponent
     # UGLY, hideous interdependencies
     #@sound = Gdx.audio.newSound(Gdx.files.internal(file));
     @sound = MainGame.instance.manager.get(file, Sound.java_class)
-    id = @sound.play(1.0)
-    @sound.setLooping(id, options[:loop] || false)
-    # @sound.dispose here?
+    if options[:loop] == true
+      id = @sound.loop(1.0)
+    else
+      id = @sound.play(1.0)
+    end
+  end
+
+  def dispose
+    @sound.dispose
   end
 end
