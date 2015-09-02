672e8e26bd476e993547c3ee499e82e5d871bd39
ashes999 <ashes999@yahoo.com>
2015-09-02 15:58:40 -0400

    Travis-CI: fix build breakages caused by empty directories

--- tools/desktop_libgdx_builder.rb
+++ tools/desktop_libgdx_builder.rb
@@ -4,14 +4,13 @@ SRC_DIR = 'src'
 class DesktopLibgdxBuilder < LibgdxBuilder
   def build(code)
     super(code)
-    move_contents "#{@output_folder}/#{SRC_DIR}", @output_folder
     move_contents "#{@output_folder}/#{CONTENT_FOLDER}", @output_folder
   end
-  
+
   private
-  
+
   # Moves contents from the src directory to the dest directory. This is like
-  # when we want to move C:\tmp\src\*.rb to C:\bin, without putting it in "C:\bin\src"  
+  # when we want to move C:\tmp\src\*.rb to C:\bin, without putting it in "C:\bin\src"
   def move_contents(src, dest)
     FileUtils::cp_r "#{src}/.", dest
     FileUtils::rm_rf src
